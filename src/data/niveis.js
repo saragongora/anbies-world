b5b6dcf59326e7e8d064163a911a8a9e708d679c
sara amandah <saragongora03@hotmail.com>
2025-08-27 20:51:19 -0300

    paginas conexo finalizadas

--- src/data/niveis.js
+++ src/data/niveis.js
@@ -65,7 +65,7 @@ export const niveis = {
     { palavra: "Akutagawa", grupo: 4, titulo: "Personagens favoritos da Ana", cor: "#FFCCEA" },
   ],
 
-4: [
+  4: [
     { palavra: "Jiung", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
     { palavra: "Taeyong", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
     { palavra: "Hanbin", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
@@ -109,8 +109,115 @@ export const niveis = {
     { palavra: "Patricia", grupo: 4, titulo: "Primas da Ana", cor: "#FFCCEA" },
   ],
 
+  6: [
+    { palavra: "A cabeça do santo", grupo: 1, titulo: "Livros que a Ana leu em 2025", cor: "#BFECFF" },
+    { palavra: "Herdeiras do mar", grupo: 1, titulo: "Livros que a Ana leu em 2025", cor: "#BFECFF" },
+    { palavra: "Nadando no escuro", grupo: 1, titulo: "Livros que a Ana leu em 2025", cor: "#BFECFF" },
+    { palavra: "O bom filho", grupo: 1, titulo: "Livros que a Ana leu em 2025", cor: "#BFECFF" },
 
+    { palavra: "Pachinko", grupo: 2, titulo: "Livros favoritados pela Ana", cor: "#CDC1FF" },
+    { palavra: "Vinte minutos e V", grupo: 2, titulo: "Livros favoritados pela Ana", cor: "#CDC1FF" },
+    { palavra: "Folha de carvalho", grupo: 2, titulo: "Livros favoritados pela Ana", cor: "#CDC1FF" },
+    { palavra: "O mar sem estrelas", grupo: 2, titulo: "Livros favoritados pela Ana", cor: "#CDC1FF" },
 
+    { palavra: "Riko", grupo: 3, titulo: "Personagens que morreram", cor: "#FFF6E3" },
+    { palavra: "Kelsier", grupo: 3, titulo: "Personagens que morreram", cor: "#FFF6E3" },
+    { palavra: "Ove", grupo: 3, titulo: "Personagens que morreram", cor: "#FFF6E3" },
+    { palavra: "Aquiles", grupo: 3, titulo: "Personagens que morreram", cor: "#FFF6E3" },
+
+    { palavra: "Seth", grupo: 4, titulo: "Personagens de All for the game", cor: "#FFCCEA" },
+    { palavra: "Abby", grupo: 4, titulo: "Personagens de All for the game", cor: "#FFCCEA" },
+    { palavra: "Jean", grupo: 4, titulo: "Personagens de All for the game", cor: "#FFCCEA" },
+    { palavra: "Neil", grupo: 4, titulo: "Personagens de All for the game", cor: "#FFCCEA" },
+  ],
+
+  7: [
+    { palavra: "Rui", grupo: 1, titulo: "Participaram do Boys Planet", cor: "#BFECFF" },
+    { palavra: "Taerae", grupo: 1, titulo: "Participaram do Boys Planet", cor: "#BFECFF" },
+    { palavra: "Keita", grupo: 1, titulo: "Participaram do Boys Planet", cor: "#BFECFF" },
+    { palavra: "Woongki", grupo: 1, titulo: "Participaram do Boys Planet", cor: "#BFECFF" },
+
+    { palavra: "Ricky", grupo: 2, titulo: "Idols que já moraram nos EUA", cor: "#CDC1FF" },
+    { palavra: "Mark", grupo: 2, titulo: "Idols que já moraram nos EUA", cor: "#CDC1FF" },
+    { palavra: "Jay", grupo: 2, titulo: "Idols que já moraram nos EUA", cor: "#CDC1FF" },
+    { palavra: "Gyuvin", grupo: 2, titulo: "Idols que já moraram nos EUA", cor: "#CDC1FF" },
+
+    { palavra: "Jiduck", grupo: 3, titulo: "Pelúcias de kpop rosa", cor: "#FFF6E3" },
+    { palavra: "Dwaekki", grupo: 3, titulo: "Pelúcias de kpop rosa", cor: "#FFF6E3" },
+    { palavra: "Kkyuping", grupo: 3, titulo: "Pelúcias de kpop rosa", cor: "#FFF6E3" },
+    { palavra: "DDEONGbyeoli", grupo: 3, titulo: "Pelúcias de kpop rosa", cor: "#FFF6E3" },
+
+    { palavra: "Quokka", grupo: 4, titulo: "Skzoos", cor: "#FFCCEA" },
+    { palavra: "Leebit", grupo: 4, titulo: "Skzoos", cor: "#FFCCEA" },
+    { palavra: "Wolf Chan", grupo: 4, titulo: "Skzoos", cor: "#FFCCEA" },
+    { palavra: "Jiniret", grupo: 4, titulo: "Skzoos", cor: "#FFCCEA" },
+  ],
+
+  8: [
+    { palavra: "Stunner", grupo: 1, titulo: "Mini albuns lançados em 2025", cor: "#BFECFF" },
+    { palavra: "Kick Out, Flip Now!", grupo: 1, titulo: "Mini albuns lançados em 2025", cor: "#BFECFF" },
+    { palavra: "Poppop", grupo: 1, titulo: "Mini albuns lançados em 2025", cor: "#BFECFF" },
+    { palavra: "No genre", grupo: 1, titulo: "Mini lbuns lançados em 2025", cor: "#BFECFF" },
+
+    { palavra: "Hot Sauce", grupo: 2, titulo: "Primeiro full album de um grupo", cor: "#CDC1FF" },
+    { palavra: "Go Live", grupo: 2, titulo: "Primeiro full album de um grupo", cor: "#CDC1FF" },
+    { palavra: "Killin'it", grupo: 2, titulo: "Primeiro full album de um grupo", cor: "#CDC1FF" },
+    { palavra: "Troubleshooting", grupo: 2, titulo: "Primeiro full album de um grupo", cor: "#CDC1FF" },
+
+    { palavra: "1999", grupo: 3, titulo: "Músicas solos do NCT", cor: "#FFF6E3" },
+    { palavra: "Tap", grupo: 3, titulo: "Músicas solos do NCT", cor: "#FFF6E3" },
+    { palavra: "Twisted Paradise", grupo: 3, titulo: "Músicas solos do NCT", cor: "#FFF6E3" },
+    { palavra: "Smoke", grupo: 3, titulo: "Músicas solos do NCT", cor: "#FFF6E3" },
+
+    { palavra: "Colors", grupo: 4, titulo: "Comeback mais recente dos grupos favoritos da Ana", cor: "#FFCCEA" },
+    { palavra: "Karma", grupo: 4, titulo: "Comeback mais recente dos grupos favoritos da Ana", cor: "#FFCCEA" },
+    { palavra: "DUH!", grupo: 4, titulo: "Comeback mais recente dos grupos favoritos da Ana", cor: "#FFCCEA" },
+    { palavra: "Beautiful Mind", grupo: 4, titulo: "Comeback mais recente dos grupos favoritos da Ana", cor: "#FFCCEA" },
+  ],
+
+  9: [
+    { palavra: "Itadori", grupo: 1, titulo: "Protagonistas de anime", cor: "#BFECFF" },
+    { palavra: "Hinata", grupo: 1, titulo: "Protagonistas de anime", cor: "#BFECFF" },
+    { palavra: "Tanjiro", grupo: 1, titulo: "Protagonistas de anime", cor: "#BFECFF" },
+    { palavra: "Eren", grupo: 1, titulo: "Protagonistas de anime", cor: "#BFECFF" },
+
+    { palavra: "Yamaguchi", grupo: 2, titulo: "Personagens de Haikyuu", cor: "#CDC1FF" },
+    { palavra: "Kenma", grupo: 2, titulo: "Personagens de Haikyuu", cor: "#CDC1FF" },
+    { palavra: "Kageyama", grupo: 2, titulo: "Personagens de Haikyuu", cor: "#CDC1FF" },
+    { palavra: "Tanaka", grupo: 2, titulo: "Personagens de Haikyuu", cor: "#CDC1FF" },
+
+    { palavra: "Maomao", grupo: 3, titulo: "Personagens de cabelo verde", cor: "#FFF6E3" },
+    { palavra: "Deku", grupo: 3, titulo: "Personagens de cabelo verde", cor: "#FFF6E3" },
+    { palavra: "Zoro", grupo: 3, titulo: "Personagens de cabelo verde", cor: "#FFF6E3" },
+    { palavra: "Maki", grupo: 3, titulo: "Personagens de cabelo verde", cor: "#FFF6E3" },
+
+    { palavra: "Naruto", grupo: 4, titulo: "Animes que a Ana assistiu", cor: "#FFCCEA" },
+    { palavra: "Wonder Egg Priorty", grupo: 4, titulo: "Animes que a Ana assistiu", cor: "#FFCCEA" },
+    { palavra: "Attack on Titan", grupo: 4, titulo: "Animes que a Ana assistiu", cor: "#FFCCEA" },
+    { palavra: "Fullmetal alchemist", grupo: 4, titulo: "Animes que a Ana assistiu", cor: "#FFCCEA" },
+  ],
+
+  10: [
+    { palavra: "Ana Laura", grupo: 1, titulo: "Nascidos em setembro", cor: "#BFECFF" },
+    { palavra: "Han", grupo: 1, titulo: "Nascidos em setembro", cor: "#BFECFF" },
+    { palavra: "Yeonjun", grupo: 1, titulo: "Nascidos em setembro", cor: "#BFECFF" },
+    { palavra: "Jooyeon", grupo: 1, titulo: "Nascidos em setembro", cor: "#BFECFF" },
+
+    { palavra: "Jaehee", grupo: 2, titulo: "Vocalistas", cor: "#CDC1FF" },
+    { palavra: "Doyoung", grupo: 2, titulo: "Vocalistas", cor: "#CDC1FF" },
+    { palavra: "Taerae", grupo: 2, titulo: "Vocalistas", cor: "#CDC1FF" },
+    { palavra: "I.N", grupo: 2, titulo: "Vocalistas", cor: "#CDC1FF" },
+
+    { palavra: "José", grupo: 3, titulo: "Personagens bíblicos", cor: "#FFF6E3" },
+    { palavra: "Sara", grupo: 3, titulo: "Personagens bíblicos", cor: "#FFF6E3" },
+    { palavra: "João", grupo: 3, titulo: "Personagens bíblicos", cor: "#FFF6E3" },
+    { palavra: "Maria", grupo: 3, titulo: "Personagens bíblicos", cor: "#FFF6E3" },
+
+    { palavra: "Feliz", grupo: 4, titulo: "Te amo <3", cor: "#FFCCEA" },
+    { palavra: "Aniversário", grupo: 4, titulo: "Te amo <3", cor: "#FFCCEA" },
+    { palavra: "Meu", grupo: 4, titulo: "Te amo <3", cor: "#FFCCEA" },
+    { palavra: "Amorzinho", grupo: 4, titulo: "Te amo <3", cor: "#FFCCEA" },
+  ],
 
 
 
