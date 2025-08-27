9a58ddee3f90ea8355d9b61e0b6c7da99abb7738
sara amandah <saragongora03@hotmail.com>
2025-08-27 12:12:26 -0300

    novos niveis adicionados

---
+++ src/data/niveis.js
@@ -0,0 +1,127 @@
+export const niveis = {
+  1: [
+    { palavra: "ISTJ", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
+    { palavra: "Boom", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
+    { palavra: "We Young", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
+    { palavra: "Reload", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
+
+    { palavra: "INFJ", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
+    { palavra: "INFP", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
+    { palavra: "ENFP", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
+    { palavra: "ENFJ", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
+
+    { palavra: "Haechan", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
+    { palavra: "Felix", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
+    { palavra: "Heesung", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
+    { palavra: "Soobin", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
+
+    { palavra: "Ricky", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
+    { palavra: "Lee Know", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
+    { palavra: "Wonwoo", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
+    { palavra: "Renjun", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
+  ],
+
+  2: [
+    { palavra: "In bloom", grupo: 1, titulo: "Musicas de debut", cor: "#BFECFF" },
+    { palavra: "Happy Death Day", grupo: 1, titulo: "Musicas de debut", cor: "#BFECFF" },
+    { palavra: "Wish", grupo: 1, titulo: "Musicas de debut", cor: "#BFECFF" },
+    { palavra: "Siren", grupo: 1, titulo: "Musicas de debut", cor: "#BFECFF" },
+
+    { palavra: "Baby blue", grupo: 2, titulo: "Bsides de 2025", cor: "#CDC1FF" },
+    { palavra: "Code Red", grupo: 2, titulo: "Bsides de 2025", cor: "#CDC1FF" },
+    { palavra: "Ghost", grupo: 2, titulo: "Bsides de 2025", cor: "#CDC1FF" },
+    { palavra: "Waves", grupo: 2, titulo: "Bsides de 2025", cor: "#CDC1FF" },
+
+    { palavra: "Red lights", grupo: 3, titulo: "Musicas com cor no nome", cor: "#FFF6E3" },
+    { palavra: "Pink venon", grupo: 3, titulo: "Musicas com cor no nome", cor: "#FFF6E3" },
+    { palavra: "Black on black", grupo: 3, titulo: "Musicas com cor no nome", cor: "#FFF6E3" },
+    { palavra: "Golden hour", grupo: 3, titulo: "Musicas com cor no nome", cor: "#FFF6E3" },
+
+    { palavra: "Hellevator", grupo: 4, titulo: "Músicas que o xdinary heroes já fez cover", cor: "#FFCCEA" },
+    { palavra: "Tomboy", grupo: 4, titulo: "Músicas que o xdinary heroes já fez cover", cor: "#FFCCEA" },
+    { palavra: "Impossible", grupo: 4, titulo: "Músicas que o xdinary heroes já fez cover", cor: "#FFCCEA" },
+    { palavra: "Love me right", grupo: 4, titulo: "Músicas que o xdinary heroes já fez cover", cor: "#FFCCEA" },
+  ],
+
+  3: [
+    { palavra: "Zhang Hao", grupo: 1, titulo: "São viados", cor: "#BFECFF" },
+    { palavra: "Haechan", grupo: 1, titulo: "São viados", cor: "#BFECFF" },
+    { palavra: "Jacinto", grupo: 1, titulo: "São viados", cor: "#BFECFF" },
+    { palavra: "Jeremy", grupo: 1, titulo: "São viados", cor: "#BFECFF" },
+
+    { palavra: "Felix", grupo: 2, titulo: "Loiros naturais", cor: "#CDC1FF" },
+    { palavra: "Jongseob", grupo: 2, titulo: "Loiros naturais", cor: "#CDC1FF" },
+    { palavra: "Felicia", grupo: 2, titulo: "Loiros naturais", cor: "#CDC1FF" },
+    { palavra: "Aaron", grupo: 2, titulo: "Loiros naturais", cor: "#CDC1FF" },
+
+    { palavra: "Ningning", grupo: 3, titulo: "Idols chineses", cor: "#FFF6E3" },
+    { palavra: "Jun", grupo: 3, titulo: "Idols chineses", cor: "#FFF6E3" },
+    { palavra: "Ricky", grupo: 3, titulo: "Idols chineses", cor: "#FFF6E3" },
+    { palavra: "Chenle", grupo: 3, titulo: "Idols chineses", cor: "#FFF6E3" },
+
+    { palavra: "Andrew", grupo: 4, titulo: "Personagens favoritos da Ana", cor: "#FFCCEA" },
+    { palavra: "Kaiser", grupo: 4, titulo: "Personagens favoritos da Ana", cor: "#FFCCEA" },
+    { palavra: "Hunter", grupo: 4, titulo: "Personagens favoritos da Ana", cor: "#FFCCEA" },
+    { palavra: "Akutagawa", grupo: 4, titulo: "Personagens favoritos da Ana", cor: "#FFCCEA" },
+  ],
+
+4: [
+    { palavra: "Jiung", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
+    { palavra: "Taeyong", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
+    { palavra: "Hanbin", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
+    { palavra: "San", grupo: 1, titulo: "Idols com tatuagem", cor: "#BFECFF" },
+
+    { palavra: "Mark", grupo: 2, titulo: "Main rappers", cor: "#CDC1FF" },
+    { palavra: "Changbin", grupo: 2, titulo: "Main rappers", cor: "#CDC1FF" },
+    { palavra: "Sakuya", grupo: 2, titulo: "Main rappers", cor: "#CDC1FF" },
+    { palavra: "Gunwook", grupo: 2, titulo: "Main rappers", cor: "#CDC1FF" },
+
+    { palavra: "Jaehee", grupo: 3, titulo: "Idols com o nome Kim", cor: "#FFF6E3" },
+    { palavra: "Seungmin", grupo: 3, titulo: "Idols com o nome Kim", cor: "#FFF6E3" },
+    { palavra: "Sunoo", grupo: 3, titulo: "Idols com o nome Kim", cor: "#FFF6E3" },
+    { palavra: "Jongseob", grupo: 3, titulo: "Idols com o nome Kim", cor: "#FFF6E3" },
+
+    { palavra: "Ricky", grupo: 4, titulo: "Idol que têm gato como animal representante", cor: "#FFCCEA" },
+    { palavra: "Riku", grupo: 4, titulo: "Idol que têm gato como animal representante", cor: "#FFCCEA" },
+    { palavra: "Jungwon", grupo: 4, titulo: "Idol que têm gato como animal representante", cor: "#FFCCEA" },
+    { palavra: "Ten", grupo: 4, titulo: "Idol que têm gato como animal representante", cor: "#FFCCEA" },
+  ],
+
+  5: [
+    { palavra: "Hollow knight", grupo: 1, titulo: "Jogos que a Ana zerou", cor: "#BFECFF" },
+    { palavra: "Enigma do medo", grupo: 1, titulo: "Jogos que a Ana zerou", cor: "#BFECFF" },
+    { palavra: "Spiritfarer", grupo: 1, titulo: "Jogos que a Ana zerou", cor: "#BFECFF" },
+    { palavra: "Tangle tower", grupo: 1, titulo: "Jogos que a Ana zerou", cor: "#BFECFF" },
+
+    { palavra: "Silent Hill", grupo: 2, titulo: "Gameplays que a Ana assistiu em 2025", cor: "#CDC1FF" },
+    { palavra: "Blue Prince", grupo: 2, titulo: "Gameplays que a Ana assistiu em 2025", cor: "#CDC1FF" },
+    { palavra: "Umineko", grupo: 2, titulo: "Gameplays que a Ana assistiu em 2025", cor: "#CDC1FF" },
+    { palavra: "Golden Idle", grupo: 2, titulo: "Gameplays que a Ana assistiu em 2025", cor: "#CDC1FF" },
+
+    { palavra: "Mia", grupo: 3, titulo: "Personagens de A Ordem Paranormal", cor: "#FFF6E3" },
+    { palavra: "Agatha", grupo: 3, titulo: "Personagens de A Ordem Paranormal", cor: "#FFF6E3" },
+    { palavra: "Carina", grupo: 3, titulo: "Personagens de A Ordem Paranormal", cor: "#FFF6E3" },
+    { palavra: "Nathalia", grupo: 3, titulo: "Personagens de A Ordem Paranormal", cor: "#FFF6E3" },
+
+    { palavra: "Cris", grupo: 4, titulo: "Primas da Ana", cor: "#FFCCEA" },
+    { palavra: "Tais", grupo: 4, titulo: "Primas da Ana", cor: "#FFCCEA" },
+    { palavra: "Isa", grupo: 4, titulo: "Primas da Ana", cor: "#FFCCEA" },
+    { palavra: "Patricia", grupo: 4, titulo: "Primas da Ana", cor: "#FFCCEA" },
+  ],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
--- src/pages/Conexo.jsx
+++ src/pages/Conexo.jsx
@@ -6,41 +6,22 @@ import clouds from "../assets/nuvens.svg";
 import background from "../assets/fundo.mp4";
 import containerConexo from "../assets/container_conexo.svg";
 
+import { niveis } from "../data/niveis";
+
 export default function Conexo() {
   const navigate = useNavigate();
   const [nivelSelecionado, setNivelSelecionado] = useState(null);
   const [palavrasSelecionadas, setPalavrasSelecionadas] = useState([]);
   const [gruposCorretos, setGruposCorretos] = useState([]);
+  const [palavrasNivel, setPalavrasNivel] = useState([]);
   const [palavrasErradas, setPalavrasErradas] = useState([]);
-  const [palavrasNivel1, setPalavrasNivel1] = useState([]);
   const [tentativas, setTentativas] = useState(0);
   const [completou, setCompletou] = useState(false);
 
-  const nivel1 = [
-    { palavra: "ISTJ", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
-    { palavra: "Boom", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
-    { palavra: "We Young", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
-    { palavra: "Reload", grupo: 1, titulo: "Titles do Dream", cor: "#BFECFF" },
-
-    { palavra: "INFJ", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
-    { palavra: "INFP", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
-    { palavra: "ENFP", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
-    { palavra: "ENFJ", grupo: 2, titulo: "MBTIs do Stray Kids", cor: "#CDC1FF" },
-
-    { palavra: "Haechan", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
-    { palavra: "Felix", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
-    { palavra: "Heesung", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
-    { palavra: "Soobin", grupo: 3, titulo: "Idols que jogam LoL", cor: "#FFF6E3" },
-
-    { palavra: "Ricky", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
-    { palavra: "Lee Know", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
-    { palavra: "Wonwoo", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
-    { palavra: "Renjun", grupo: 4, titulo: "Idols que são INTP", cor: "#FFCCEA" },
-  ];
-
+  // embaralhar quando escolher nível
   useEffect(() => {
-    if (nivelSelecionado === 1) {
-      setPalavrasNivel1([...nivel1].sort(() => Math.random() - 0.5));
+    if (nivelSelecionado) {
+      setPalavrasNivel([...niveis[nivelSelecionado]].sort(() => Math.random() - 0.5));
     }
   }, [nivelSelecionado]);
 
@@ -53,16 +34,16 @@ export default function Conexo() {
   };
 
   useEffect(() => {
-    if (palavrasSelecionadas.length === 4) {
+    if (palavrasSelecionadas.length === 4 && nivelSelecionado) {
       setTentativas((prev) => prev + 1);
 
       const grupos = palavrasSelecionadas.map(
-        (p) => nivel1.find((obj) => obj.palavra === p).grupo
+        (p) => niveis[nivelSelecionado].find((obj) => obj.palavra === p).grupo
       );
 
       if (grupos.every((g) => g === grupos[0])) {
         const grupoId = grupos[0];
-        const palavrasDoGrupo = nivel1.filter((obj) => obj.grupo === grupoId);
+        const palavrasDoGrupo = niveis[nivelSelecionado].filter((obj) => obj.grupo === grupoId);
         const { titulo, cor } = palavrasDoGrupo[0];
 
         setGruposCorretos((prev) => {
@@ -114,18 +95,18 @@ export default function Conexo() {
       <motion.div className="relative z-10 w-[900px] max-w-[95%] drop-shadow-2xl">
         <img src={containerConexo} alt="container conexo" className="w-full" />
 
-        {/* contador de tentativas no topo do caderno */}
-        {nivelSelecionado === 1 && (
+        {/* contador de tentativas */}
+        {nivelSelecionado && (
           <p className="absolute top-[130px] right-[50px] text-lg font-semibold text-gray-700 z-20">
             Tentativas: {tentativas}
           </p>
         )}
 
-        {/* Conteúdo do jogo dentro do caderno */}
+        {/* Conteúdo do jogo */}
         <div className="absolute top-[170px] left-1/2 -translate-x-1/2 w-[800px] max-w-[90%] flex flex-col items-center">
           {!nivelSelecionado && (
             <div className="grid grid-cols-5 gap-x-12 gap-y-10 justify-items-center w-full px-8 translate-y-6">
-              {Array.from({ length: 10 }, (_, i) => (
+              {Array.from({ length: Object.keys(niveis).length }, (_, i) => (
                 <button
                   key={i}
                   onClick={() => setNivelSelecionado(i + 1)}
@@ -135,10 +116,9 @@ export default function Conexo() {
                 </button>
               ))}
             </div>
-
           )}
 
-          {nivelSelecionado === 1 && (
+          {nivelSelecionado && (
             <div className="flex flex-col gap-4 w-full items-center relative">
               {/* grupos corretos */}
               {gruposCorretos.map((grupo) => (
@@ -156,12 +136,9 @@ export default function Conexo() {
                 </motion.div>
               ))}
 
-
-
-
               {/* grade de palavras */}
               <div className="grid grid-cols-4 gap-4 p-6 w-full">
-                {palavrasNivel1
+                {palavrasNivel
                   .filter((obj) => !gruposCorretos.some((g) => g.id === obj.grupo))
                   .map((obj, idx) => {
                     const selecionada = palavrasSelecionadas.includes(obj.palavra);
@@ -173,42 +150,38 @@ export default function Conexo() {
                         onClick={() => selecionarPalavra(obj.palavra)}
                         animate={
                           errada
-                            ? { backgroundColor: "#e74444", color: "#fff" } // vermelho
+                            ? { backgroundColor: "#e74444", color: "#fff" }
                             : selecionada
-                              ? { backgroundColor: "#5289b8", color: "#fff" } // azul selecionado
-                              : { backgroundColor: "#e5e7eb", color: "#333" } // cinza normal
+                            ? { backgroundColor: "#5289b8", color: "#fff" }
+                            : { backgroundColor: "#e5e7eb", color: "#333" }
                         }
                         transition={{ duration: 0.5, ease: "easeOut" }}
                         className="flex items-center justify-center px-3 py-4 rounded-lg font-semibold text-center"
                       >
                         {obj.palavra}
                       </motion.button>
-
                     );
                   })}
               </div>
 
-             {completou && (
-  <motion.div
-    initial={{ scale: 0.8, opacity: 0 }}
-    animate={{ scale: 1, opacity: 1 }}
-    transition={{ duration: 0.8 }}
-    className="fixed inset-0 flex items-center justify-center z-50"
-    onClick={() => setCompletou(false)} // fecha ao clicar no fundo
-  >
-    <div
-      className="bg-white rounded-xl shadow-lg px-10 py-12"
-      onClick={(e) => e.stopPropagation()} // evita fechar se clicar dentro
-    >
-      <p className="text-lg font-semibold text-[#5289b8] text-center">
-        Parabéns! Você completou em {tentativas} tentativas!
-      </p>
-    </div>
-  </motion.div>
-)}
-
-
-
+              {completou && (
+                <motion.div
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ duration: 0.8 }}
+                  className="fixed inset-0 flex items-center justify-center z-50"
+                  onClick={() => setCompletou(false)}
+                >
+                  <div
+                    className="bg-white rounded-xl shadow-lg px-10 py-12"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <p className="text-lg font-semibold text-[#5289b8] text-center">
+                      Parabéns! Você completou em {tentativas} tentativas!
+                    </p>
+                  </div>
+                </motion.div>
+              )}
             </div>
           )}
         </div>
@@ -218,10 +191,8 @@ export default function Conexo() {
       <motion.button
         onClick={() => {
           if (nivelSelecionado === null) {
-            // Está no menu de níveis → volta para home
             navigate("/");
           } else {
-            // Está dentro de um nível → volta para o menu de níveis
             setNivelSelecionado(null);
             setPalavrasSelecionadas([]);
             setGruposCorretos([]);
@@ -233,7 +204,6 @@ export default function Conexo() {
       >
         ← Voltar
       </motion.button>
-
     </div>
   );
 }
